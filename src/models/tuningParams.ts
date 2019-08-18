474adc1cc809f28442f9264bbd56c2610193344c
onoroff <adam.grunke@gmail.com>
2019-08-18 13:15:35 -0700

    all files weren't saved on last commit

--- src/models/tuningParams.ts
+++ src/models/tuningParams.ts
@@ -1,19 +1,23 @@
 import mongoose, {Schema} from 'mongoose'
 
 const tuningSchema = new Schema({
-    artistSeed: {
+    seed_artists: {
         type: String,
         required: [true, 'You need to have a seed for artist']
     }, 
-    genreSeed: {
+    seed_genre: {
         type: String,
         required: [true, 'You need to have a seed for artist']
     },
-    songSeed: {
+    seed_tracks: {
         type: String,
         required: [true, 'You need to have a seed for artist']
     }, 
-    
+    limit: Number,
+    danceability: Number,
+    energy: Number,
+    instrumentalness: Number,
+    valence: Number
 })
 
 export default mongoose.model('Tuning', tuningSchema);
